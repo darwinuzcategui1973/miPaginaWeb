335ea49bdf03dfc9a00375bc3a939e9c98d227e8
Darwin Uzcategui Gmail <darwin.uzcategui1973@gmail.com>
2024-01-23 18:01:10 -0400

    prueba flutter

---
+++ darwindocs/index.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <title>Pagina web Darwin Felipe Uzcategui</title>
+    <link rel="stylesheet" href="estilo/estilo.css">
+    <!-- El link externo de mi fuente -->
+    <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet">
+    <!-- para colocar carulese -->
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <!--mi estiqueta para que es mi pagina  ESTA DOCS-->
+    <meta name="google-site-verification" content="jAFLY0kyGdoOUhdPaCNMPzMbDRwUIzqJ0sLby0N-dSo" />
+</head>
+
+<body>
+    <a id="whatsapp" href="https://api.whatsapp.com/send?phone=584149213235"><img src="imagenes/whatsapp-64.png" alt=""></a>
+    <header>
+        <!-- Va nuestro menu de nuestra pagina -->
+        <div class="menu">
+            <div class="contenedor">
+                <p class="logo">DARWIN UZCATEGUI</p>
+
+                <img class="menu-icon" src="imagenes/menu.png" alt="">
+                <nav>
+                    <ul class="lista-menu">
+                        <li><a href="#acerca">Acerca de mi..</a></li>
+                        <li><a href="#servicios">Servicio</a></li>
+                        <li><a href="#mis-trabajos">Mis Trabajo</a></li>
+                        <li><a href="#por-que-trabajar">Porque Trabjar conmigo</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#contacto">Contacto</a></li>
+
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+        <div class="contenedor" id="contenedor-titulo-flex">
+
+
+            <div class="contenedor-titulo">
+                <h1>Darwin Felipe Uzcátegui González</h1>
+                <h2>Programador & Desarrollador Movil</h2>
+                <a href="#mis-trabajos">VER MI TRABAJO</a>
+
+            </div>
+        </div>
+
+    </header>
+    <!-- la etiqueta para ceo main en lo principal para google estienda -->
+
+    <main>
+        <section id="acerca">
+            <div class="contenedor">
+                <h3>Acerca de mi</h3>
+                <div class="contenedor-acerca">
+                    <div class="computadora">
+                        <img src="imagenes/curso.jpg">
+                    </div>
+                    <div class="texto">
+                        <p>Mi nombre es <strong>Darwin Felipe Uzcátegui González</strong> y soy un programador especializado en sistemas de escritorio. Poseo experiencia destacada en el desarrollo de sistemas de facturación fiscal diseñados específicamente para satisfacer las necesidades de pequeñas y medianas empresas, así como en el desarrollo de sistemas administrativos. <strong>Mi enfoque se centra en ofrecer soluciones tecnológicas eficientes y personalizadas para optimizar los procesos empresariales</strong> .</p>
+                        <p>Garantizo la implementación constante de las tecnologías de desarrollo más avanzadas para crear aplicaciones de escritorio, móviles y web. Si estás en busca de un sistema de facturación fiscal, administrativo o una página web moderna y funcional, adaptada a todos los dispositivos y pantallas, <strong>has llegado al lugar indicado</strong> . Mi enfoque se centra en proporcionar soluciones tecnológicas vanguardistas que satisfacen tus necesidades específicas.</p>
+                        <p>Para establecer contacto, te ofrezco diversas opciones: puedes encontrarme en <strong> Twitter, Linkedin e Instagram</strong> ,  llamarme al número <strong>+58 0414 921 3235 </strong> , o simplemente completar el formulario de contacto ubicado al final de mi sitio web. Además, <strong> también puedes contactarme a través de WhatsApp</strong> . Estoy aquí para responder a tus preguntas y atender tus necesidades.</p>
+                        <a href="https://astounding-dusk-ca43e7.netlify.app/">VER MI PORTAFOLIO</a>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <section id="servicios">
+                <div class="contenedor">
+                    <h3>Servicios</h3>
+                    <div class="contenedor-servicios">
+                        <div class="servicio naranja">
+                            <h4>Diseño Web</h4>
+                            <p>Páginas web atractivas y funcionales con el objetivo de satisfacer todas tus expectativas. La importancia del diseño es fundamental para tener éxito y atraer a tus visitantes. También lo es la velocidad de carga del sitio web,
+                                deben ser rápidos para que el visitante tenga la mejor experiencia</p>
+                            <img class="icono" src="imagenes/1.png">
+                            <img class="ondas" src="imagenes/waves.png">
+                        </div>
+                        <div class="servicio celeste">
+                            <h4>Responsive Design</h4>
+                            <p>Hoy en día las páginas web se ven en multitud de dispositivos como tablets, celulares, libros electrónicos, notebooks, PCs, etc. La tecnología móvil es el presente y el futuro. Para eso es importante y me aseguro de que tu
+                                sitio web sea compatible con todos los tamaños de pantalla de esta época moderna</p>
+                            <img class="icono" src="imagenes/9.png">
+                            <img class="ondas" src="imagenes/waves.png">
+                        </div>
+                        <div class="servicio naranja">
+                            <h4>SEO</h4>
+                            <p>Al crear sitios web, tengo muy en cuenta la optimización de motores de búsqueda (SEO). Llevo a cabo prácticas de SEO estándar en cada página del sitio web, lo que permite que tu sitio tenga una clasificación más alta en los
+                                resultados de Google y asi generar mas tráfico a la web</p>
+                            <img class="icono" src="imagenes/10.png">
+                            <img class="ondas" src="imagenes/waves.png">
+                        </div>
+                        <div class="servicio celeste">
+                            <h4>Autoadministrable</h4>
+                            <p>Es importante que puedas administrar tu página web vos mismo, desde cualquier lugar y en cualquier momento. Desarrollo sistemas de gestión de contenido que te permiten ver los datos de tu página web y editar el contenido que
+                                quieras, lo que te permite mantenerte actualizado
+                            </p>
+                            <img class="icono" src="imagenes/4.png">
+                            <img class="ondas" src="imagenes/waves.png">
+                        </div>
+                        <div class="servicio naranja">
+                            <h4>Mantenimiento</h4>
+                            <p>Podés contactarme si necesitas actualizar tu página web, agregar nuevas secciones o cualquier cambio que necesites. Esto es perfecto para los sitios web de pequeñas empresas, donde no se necesita un diseñador de páginas web
+                                a tiempo completo, sino solo en distintas ocasiones</p>
+                            <img class="icono" src="imagenes/5.png">
+                            <img class="ondas" src="imagenes/waves.png">
+                        </div>
+                        <div class="servicio celeste">
+                            <h4>Marketing Digital</h4>
+                            <p>Utilizo las herramientas mas importantes como Google Adwords y Google Analytics, para que tu negocio aparezca en los resultados de búsqueda de Google justo cuando alguien te necesita y analizar como los usuarios interactúan
+                                con tu página web</p>
+                            <img class="icono" src="imagenes/6.png">
+                            <img class="ondas" src="imagenes/waves.png">
+                        </div>
+                    </div>
+                </div>
+                <section id="mis-trabajos">
+                    <div class="contenedor">
+                        <h3>Mis Trabajos</h3>
+                        <div class="owl-carousel owl-theme">
+
+
+                            <div class="item">
+                                <a target="_blank" href="https://drive.google.com/file/d/0B7OlNZqShyA_LXlJUnFGdjVvTHc/view">
+                                    <img src="imagenes/gmdpto.png">
+                                    <p>sistemas de facturacion Fiscal</p>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a target="_blank" href="https://www.youtube.com/channel/UCxEuhaaMVJ6mQ1KDEvxp-IA?view_as=subscriber">
+                                    <img src="imagenes/curso3.png">
+                                    <p>Curso de progrmacion para niños y chamos</p>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a target="_blank" href="https://play.google.com/store/apps/details?id=com.gmdsistema.darwinuzcategui.hilospersitenciadatossonidos&hl=es_VE">
+                                    <img src="imagenes/pelotaGmd.png">
+                                    <p>Juego Pelota Playera GMD</p>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a target="_blank" href="https://darwinuzcategui.github.io/portafolioGmd/#/inicio">
+                                    <img src="imagenes/portafolio.png">
+                                    <p>Diseño de Pagina Web Catalogo Prouctos</p>
+                                </a>
+                            </div>
+                            <div class="item">
+                                <a target="_blank" href="https://play.google.com/store/apps/details?id=com.gmdsistema.darwinuzcategui.hilospersitenciadatossonidos&hl=es_VE">
+                                    <img src="imagenes/factura.png">
+                                    <p>Sistema de Facturacón</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="por-que-trabajar">
+                    <div class="contenedor">
+                        <h3>¿Por que trabajar conmigo?</h3>
+                        <div class="contenedor-trabajar">
+                            <div class="item">
+                                <ul>
+                                    <li><img src="imagenes/velocidad.png" alt=""></li>
+                                    <li>Velocidad</li>
+                                    <li>Me aseguro de que tu web cargue entre 2 y 4 segundos (depende de la cantidad de contenido del sitio). Si una web tarda más de 5 segundos en cargar, el 70% de los usuarios lo cierra y no vuelve a entrar más</li>
+                                </ul>
+                            </div>
+                            <div class="item">
+                                <ul>
+                                    <li><img src="imagenes/mantenimiento.png" alt=""></li>
+                                    <li>Mantenimiento</li>
+                                    <li>Mantengo una comunicación constante con mi cliente, para enseñarle a modificar contenido mediante el panel de Wordpress o para cualquier duda/cambio que quiera hacer en su sitio web</li>
+                                </ul>
+                            </div>
+                            <div class="item">
+                                <ul>
+                                    <li><img src="imagenes/mobile2.png" alt=""></li>
+                                    <li>Mobile Priority</li>
+                                    <li>Cada vez mas personas navegan desde sus celulares en cualquier parte del mundo. Me aseguro de que tu web esté optimizada para cualquier tipo de dispositivo móvil
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section id="blog">
+                    <div class="contenedor">
+                        <h3>Blog</h3>
+                        <div class="contenedor-publicaciones">
+                            <div class="publicacion">
+                                <img src="imagenes/blog1.jpg" alt="">
+                                <div class="contenido">
+                                    <h4>¿Qué es GPT y cómo funciona?</h4>
+                                    <p>ChatGPT es una aplicación de chatbot de inteligencia artificial desarrollado en 2022 por OpenAI que se especializa en el diálogo. El chatbot es un modelo de lenguaje ajustado con técnicas de aprendizaje tanto supervisadas como de refuerzo.​ </p>
+                                    <a href="https://chat.openai.com/">Leer mas ➝</a>
+                                </div>
+                            </div>
+                            <div class="publicacion">
+                                <img src="imagenes/blog2.jpg" alt="">
+                                <div class="contenido">
+                                    <h4>¿Qué es el remarketing y como utilizarlo?</h4>
+                                    <p>El remarketing es una de las técnicas mas utlizadas por las mejores companías! ¿Querés saber de que se trata?</p>
+                                    <a href="https://advertising.amazon.com/es-mx/library/guides/remarketing#:~:text=El%20remarketing%20se%20refiere%20a,pueda%20interesar%2C%20como%20la%20conversi%C3%B3n.">Leer mas ➝</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section id="que-esperas">
+                    <div class="contenedor">
+                        <h3>¿Qué esperas para tener tu página web?</h3>
+                        <a href="">CHARLEMOS</a>
+                    </div>
+                </section>
+
+                <section id="contacto">
+                    <?php
+                        if(@$_GET['i']=='ok') { // QUIERE DECIR QUE EL FORMULARIO SE ENVIO CORRECTAMENTE ?>
+
+                        <h3>La consulta se envio correctamente. Nos contactaremos a la brevedad.</h3>
+
+                        <?php
+                        } else{
+                        ?>
+                            <div class="contenedor">
+                                <h3>Contacto</h3>
+                                <form action="envio-formulario.php" method="post" >
+                                    <input type="text" placeholder="Nombre" name="nombre" required>
+                                    <input type="email" placeholder="Email" name="email" required>
+                                    <textarea placeholder="Mensaje" name="mensaje" required></textarea>
+                                    <input type="submit" value="ENVIAR MENSAJE">
+                                </form>
+                                <?php } ?>
+
+
+                                <div class="contacto-info">
+                                    <div class="mail"><img src="imagenes/email-11-48.png" alt="">darwin.uzcategui1973@gmail.com
+                                    </div>
+                                    <div class="whatsapp"><img src="imagenes/whatsapp-48.png" alt="">+584149213235</div>
+                                </div>
+
+
+                            </div>
+                </section>
+
+    </main>
+
+    <footer>
+        <div class="contenedor">
+            <div class="redes">
+                <a target="_blank" href="https://www.facebook.com/pg/Pelota-De-Playa-multiidiomas-1519164658133365/posts/"><img src="imagenes/facebook32.png" alt="imagen de facebook"></a>
+                <a target="_blank" href="https://twitter.com/darwinuzcategui"><img src="imagenes/twitter.png" alt="imagen de twitter"></a>
+                <a target="_blank" href="https://www.instagram.com/darwinuzcategui_programador/?hl=es"><img src="imagenes/instagram64.png" alt="imagen de instagram"></a>
+                <a target="_blank" href="https://www.youtube.com/channel/UCxEuhaaMVJ6mQ1KDEvxp-IA"><img src="imagenes/youtube61.png" alt="imagen de youtube"></a>
+                <a target="_blank" href="https://github.com/darwinuzcategui"><img src="imagenes/github64.png" alt="imagen de github"></a>
+                <a target="_blank" href="https://ve.linkedin.com/in/darwin-felipe-uzc%C3%A1tegui-gonz%C3%A1lez-08a46310a"><img src="imagenes/linkedin64.png" alt="imagen de linkedin"></a>
+            </div>
+            <div class="parrafo">
+                <p>© 2019 Darwin Felipe Uzcátegui González | Programador de Sistemas Fiscales. Todos los derechos reservados</p>
+            </div>
+        </div>
+    </footer>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="owl/owl.carousel.min.js"></script>
+    <script>
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        })
+    </script>
+
+    <script>
+        $(document).ready(function() {
+
+            $(window).scroll(function() {
+                scroll = $(window).scrollTop();
+                if (scroll > 100) {
+                    $('.menu').css({
+                        "position": "fixed"
+                    });
+                    $('.menu').css({
+                        "width": "100%"
+                    });
+                    $('.menu').css({
+                        "top": "0"
+                    });
+                    $('.menu').css({
+                        "background": "#fff"
+                    });
+                    $('.menu a').css({
+                        "color": "#000"
+                    });
+                    $('.logo').css({
+                        "color": "#000"
+                    });
+                    $('.menu').css({
+                        "box-shadow": "rgba(0, 0, 0, 0.22) 6px 1px 1px"
+                    });
+                    $('.menu').css({
+                        "z-index": "100"
+                    });
+                } else {
+                    $('.menu').css({
+                        "position": "relative"
+                    });
+                    $('.menu').css({
+                        "background": "transparent"
+                    });
+                    $('.menu').css({
+                        "box-shadow": "0 0 0"
+                    });
+                    $('.menu a').css({
+                        "color": "#fff"
+                    });
+                    $('.logo').css({
+                        "color": "#fff"
+                    });
+                }
+            })
+
+            $('.menu-icon').click(function() {
+                $('header nav').slideToggle();
+            })
+
+        })
+    </script>
+
+
+</body>
+
+</html>
